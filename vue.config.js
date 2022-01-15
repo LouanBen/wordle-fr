b96a7c712d540f50ce9ea913f0abf8808b91e479
richie3366 <richie3366@gmail.com>
2022-01-15 18:39:03 +0100

    Autogenerate build_id.json

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const fs = require('fs')
+
 process.env.VUE_APP_BUILD_ID = process.env.BUILD_ID
+fs.writeFileSync('public/build_id.json', JSON.stringify({
+    BUILD_ID: process.env.BUILD_ID
+}))
 
 module.exports = {
-    
+
 }
