d20ffc7bd81e6fea37f47720c16281e473840dfe
Loji <louan.ben@gmail.com>
2022-01-18 18:31:30 +0100

    inté définition

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -156,7 +156,12 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="soluce" v-if="finished">Le mot était : <strong>{{ wordOfTheDay }}</strong> (<a :href="`https://1mot.net/${this.wordOfTheDay.toLowerCase()}`" target="_blank">définition</a>)</div>
+                        <div class="soluce" v-if="finished">Le mot était : 
+                            <strong>{{ wordOfTheDay }}</strong> 
+                            <a :href="`https://1mot.net/${this.wordOfTheDay.toLowerCase()}`" target="_blank" class="definition-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Définition</title><path d="M256 56C145.72 56 56 145.72 56 256s89.72 200 200 200 200-89.72 200-200S366.28 56 256 56zm0 82a26 26 0 11-26 26 26 26 0 0126-26zm48 226h-88a16 16 0 010-32h28v-88h-16a16 16 0 010-32h32a16 16 0 0116 16v104h28a16 16 0 010 32z"/></svg>
+                            </a>
+                        </div>
                         <div class="modal-footer" v-if="finished">
                             <div class="next-in">Prochain mot dans : <strong class="time">{{ countdownToNextWord }}</strong></div>
                             <div class="separator"></div>
@@ -825,9 +830,16 @@ export default {
                                         background: #538D4E
                 .soluce
                     margin-top: 24px
-                    a
-                        color: white
-                        text-decoration: none
+                    display: flex
+                    strong
+                        padding-left: 4px
+                        display: block
+                    .definition-icon
+                        svg
+                            width: 1em
+                            height: 1em
+                            margin-left: 8px
+                            fill: #919191
                 .modal-footer
                     display: flex
                     width: 100%
