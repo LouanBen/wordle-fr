2691ff8c3529d9098b8409ce2960f4a6082c0c44
Raphaël Hervier <richie3366@gmail.com>
2022-05-05 22:05:38 +0200

    Use random build_id if not already set

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin');
 const webpack = require('webpack');
+const crypto = require('crypto');
 
 const currentYear = new Date().getFullYear();
 
-if (!process.env.BUILD_ID && process.env.NODE_ENV === 'development')
-process.env.BUILD_ID = 'development';
+if (!process.env.BUILD_ID)
+  if (process.env.NODE_ENV === 'development')
+    process.env.BUILD_ID = 'development';
+  else
+    process.env.BUILD_ID = crypto.randomBytes(32).toString('hex').padStart(64, '0');
 
-process.env.VUE_APP_BUILD_ID = process.env.BUILD_ID;
+process.env.VUE_APP_BUILD_ID = process.env.BUILD_ID
 fs.writeFileSync('public/build_id.json', JSON.stringify({
   BUILD_ID: process.env.BUILD_ID
 }));
